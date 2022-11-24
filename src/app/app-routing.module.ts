b5bcfd59e401fd4673ecd0fc5b043585c4fe714f
hanaeFSDM <hanae.haiki@usmba.ac.ma>
2022-11-24 10:55:54 +0100

    forms reactive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultantInfoComponent } from './consultant-info/consultant-info.component';
 
 const routes: Routes = [];
 
